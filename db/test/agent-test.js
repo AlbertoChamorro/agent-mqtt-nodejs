81a5d80243a3f0b69b6a95698b7e9acebc11c3a4
Alberto Chamorro <albertchc04@gmail.com>
2018-08-05 13:17:49 -0600

    creation stubs with sinon for tests

--- db/test/agent-test.js
+++ db/test/agent-test.js
@@ -1,20 +1,47 @@
 'use strict'
 
 const test = require('ava')
-let db = null
-let config = {
-  logging: function () {
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
 
-  }
+let sandbox = null
+let MetricStub = {
+  belongsTo: sinon.spy()
 }
+let AgentStub = null
+
+let config = {
+  logging: function () {}
+}
+let db = null
 
 test.beforeEach(async () => {
-  const setupDatabase = require('../')
+  sandbox = sinon.sandbox.create()
+
+  AgentStub = {
+    hasMany: sandbox.spy()
+  }
+
+  const setupDatabase = proxyquire('../', {
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub
+  })
+
   db = await setupDatabase(config)
 })
 
+test.afterEach(() => {
+  sandbox && sinon.restore()
+})
+
 test('make it pass', async t => {
-  // t.pass()
-  console.log(db)
   t.truthy(db.Agent, 'Agent services should exist')
+})
+
+test.serial('Setup db', async t => {
+  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed')
+  t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricStub')
+
+  t.true(MetricStub.belongsTo.called, 'MetricStub.belongsTo was executed')
+  t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentStub')
 })
