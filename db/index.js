75bcddc08bc2f0119d8dc035585ef753cb09e479
Alberto Chamorro <albertchc04@gmail.com>
2018-08-07 07:18:17 -0600

    create services metric

--- db/index.js
+++ db/index.js
@@ -6,6 +6,7 @@ const setupAgentModel = require('./models/agent')
 const setupMetricModel = require('./models/metric')
 
 const setupAgent = require('./lib/agent')
+const setupMetric = require('./lib/metric')
 
 module.exports = async function (config) {
   config = defaults(config, {
@@ -35,7 +36,7 @@ module.exports = async function (config) {
   }
 
   const Agent = setupAgent(AgentModel)
-  const Metric = {}
+  const Metric = setupMetric(MetricModel)
   const User = {}
 
   return {
