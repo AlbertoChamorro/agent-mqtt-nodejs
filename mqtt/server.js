951fdde27321fa77797191f37c669977cc2c1956
Alberto Chamorro <albertchc04@gmail.com>
2018-08-08 15:00:13 -0600

    event emit, (mqtt console) npx

--- mqtt/server.js
+++ mqtt/server.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// const debug = require('debug')('academy:mqtt')
+const debug = require('debug')('academy:mqtt')
 const chalk = require('chalk')
 const mosca = require('mosca')
 const redis = require('redis')
@@ -18,6 +18,33 @@ const settings = {
 
 const server = new mosca.Server(settings)
 
+server.on('clientConnected', client => {
+  debug(`Client Connected: ${client.id}`)
+})
+
+server.on('clientDisconnected', client => {
+  debug(`Client Disconnected: ${client.id}`)
+})
+
+// npx mqtt -v
+// mqtt pub -t 'agent/message' -h localhost -m 'hello academy app'
+// -t: topic || -m: message
+server.on('published', (packet, client) => {
+  debug(`Received: ${packet.topic}`)
+  debug(`Payload: ${packet.payload}`)
+})
+
 server.on('ready', () => {
   console.log(`${chalk.green('[academy-mqtt]')} server is running`)
 })
+
+server.on('error', handleFatalError)
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
