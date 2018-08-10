21f5c51baeaadafd024a5d793ee04228913e452e
Alberto Chamorro <albertchc04@gmail.com>
2018-08-10 14:47:53 -0600

    create agent emitter

---
+++ agent/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const EventEmitter = require('events')
+
+class AcademyAgent extends EventEmitter {
+
+    constructor () {
+        super ()
+    }
+}
+
+module.exports = AcademyAgent
