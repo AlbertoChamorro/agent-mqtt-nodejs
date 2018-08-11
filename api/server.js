118c06b9528b14df32905515358adb9d6490ac8b
Alberto Chamorro <albertchc04@gmail.com>
2018-08-10 19:11:18 -0600

    create server with express

---
+++ api/server.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const chalk = require('chalk')
+const http = require('http')
+const express = require('express')
+
+const port = process.env.PORT || 3000
+const app = express()
+const server = http.createServer(app)
+
+server.listen(port, () => {
+    console.log(`${chalk.green('[academy-api]')} server listening on port ${port}`)
+})
