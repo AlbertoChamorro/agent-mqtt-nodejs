309b4e16a00176175eea73cf13ddf2053f7940e5
Alberto Chamorro <albertchc04@gmail.com>
2018-08-10 20:28:34 -0600

    router with express

--- api/server.js
+++ api/server.js
@@ -4,9 +4,13 @@ const chalk = require('chalk')
 const http = require('http')
 const express = require('express')
 
+const routes = require('./api')
+
 const port = process.env.PORT || 3000
 const app = express()
 const server = http.createServer(app)
+
+app.use('/api', routes)
 
 server.listen(port, () => {
     console.log(`${chalk.green('[academy-api]')} server listening on port ${port}`)
