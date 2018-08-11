3a0e73afd6e7791750ce245305fbee29c0c1e118
Alberto Chamorro <albertchc04@gmail.com>
2018-08-11 12:10:17 -0600

    implement test with sinon for created stubs

--- api/tests/api-test.js
+++ api/tests/api-test.js
@@ -2,8 +2,41 @@
 
 const test = require('ava')
 const request = require('supertest')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
 
-const server = require('../server')
+let sandbox = null
+let server = null
+let dbStub = null
+let AgentStub = {}
+let MetricStub = {}
+
+let AgentFixtures = require('./fixtures/agent')
+
+test.beforeEach(async () => {
+  sandbox = sinon.createSandbox()
+
+  dbStub = sandbox.stub()
+  dbStub.returns(Promise.resolve({
+    Agent: AgentStub,
+    Metric: MetricStub
+  }))
+
+  const api = proxyquire('../api', {
+    'db': dbStub
+  })
+
+  server = proxyquire('../server', {
+    './api': api
+  })
+
+  AgentStub.findConnected = sinon.stub()
+  AgentStub.findConnected.returns(Promise.resolve(AgentFixtures.connected))
+})
+
+test.afterEach(async () => {
+  sandbox && sinon.restore()
+})
 
 test.serial.cb('api/agents', t => {
   request(server)
@@ -12,8 +45,19 @@ test.serial.cb('api/agents', t => {
     .expect('Content-type', /json/)
     .end((err, res) => {
       t.falsy(err, 'should not return an error')
-      let body = res.body
-      t.deepEqual(body, {}, 'response body should be the expected')
+      let body = JSON.stringify(res.body)
+      let expected = JSON.stringify(AgentFixtures.connected)
+
+      t.deepEqual(body, expected, 'response body should be the expected')
       t.end() // when used test.cb()
     })
 })
+
+test.serial.todo('/api/agents/:uuid')
+test.serial.todo('/api/agents/:uuid - not found')
+
+test.serial.todo('/api/metrics/:uuid')
+test.serial.todo('/api/metrics/:uuid - not found')
+
+test.serial.todo('/api/metrics/:uuid/:type')
+test.serial.todo('/api/metrics/:uuid/:type - not found')
