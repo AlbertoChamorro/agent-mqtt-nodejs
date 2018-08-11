dbf72da4c0073423ff2fcf435a29cc216df41505
Alberto Chamorro <albertchc04@gmail.com>
2018-08-11 09:15:33 -0600

    middleware handle error

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const debug = require('debug')('academy:api')
 const chalk = require('chalk')
 const http = require('http')
 const express = require('express')
@@ -11,6 +12,27 @@ const app = express()
 const server = http.createServer(app)
 
 app.use('/api', routes)
+
+// Express Error Handler
+app.use((err, req, res, next) => {
+    debug(`${chalk.red('[error]:')} ${err.message}`)
+
+    if (err.message.match(/not found/)) {
+        return res.status(404).send({ error: err.message })
+    }
+
+    res.status(500).send({ error: err.message })
+})
+
+function handleFatalError () {
+    console.error(`${chalk.red('[fatal error]:')} ${err.message}`)
+    console.error(err.stack)
+
+    process.exit(1)
+}
+
+process.on('onCaughtException', handleFatalError)
+process.on('onHandledRejection', handleFatalError)
 
 server.listen(port, () => {
     console.log(`${chalk.green('[academy-api]')} server listening on port ${port}`)
